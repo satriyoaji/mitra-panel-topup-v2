fd2502ea89190a0943f0603ba084b2f599ed06ea
Riza Bachtiar <riza.bachtiar.rb@gmail.com>
2023-12-01 16:12:46 +0700

    fix(auth): redirect to home if authenticated

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,28 @@
-export { default } from 'next-auth/middleware'
+import { getToken } from 'next-auth/jwt';
+import { withAuth } from 'next-auth/middleware';
+import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 
 export const config = { matcher: ['/profile'] }
+
+export default async function middleware(req: NextRequest, event: NextFetchEvent) {
+    const token = await getToken({ req });
+    const isAuthenticated = !!token;
+
+    if (!isAuthenticated) {
+        return NextResponse.redirect(new URL('/auth/login', req.url));
+    }
+
+    if (req.nextUrl.pathname.startsWith('/auth') && isAuthenticated) {
+        return NextResponse.redirect(new URL('/', req.url));
+    }
+
+    const authMiddleware = await withAuth({
+        pages: {
+            signIn: `/auth/login`,
+            newUser: '/auth/register'
+        },
+    });
+
+    // @ts-expect-error
+    return authMiddleware(req, event);
+}
