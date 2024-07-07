f5bb17a93ea5415ee85e897ea42b28935472f4f1
Riza Bachtiar <riza.bachtiar.rb@gmail.com>
2024-07-07 20:59:34 +0700

    style: add game detail v2

---
+++ src/app/games/[slug]/(v2)/V2DetailCategory.tsx
@@ -0,0 +1,84 @@
+import React from "react";
+import Header from "../header";
+import ProductList from "../(product)/product-list";
+import Payment from "../(payment-method)/payment";
+import { Card, CardContent } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import Promo from "../(promo)/promo-list";
+import FormConfirmation from "../(account-confirmation)/form-confirmation";
+import CheckoutAction from "../(checkout)/checkout-action";
+import { IUseCategoryData } from "../useCategory";
+import FormAccount from "../(form-id)/form-account";
+
+function V2DetailCategory(props: IUseCategoryData) {
+    if (props.data.category !== null && props.data.category !== undefined)
+        return (
+            <div className="md:container md:px-20 pt-2 px-4">
+                <div className="">
+                    <div>
+                        <Header category={props.data.category} />
+                    </div>
+                </div>
+                <div className="md:grid md:grid-cols-5 md:gap-4 mt-1.5">
+                    <div className="col-span-3">
+                        {props.data.category.forms ? (
+                            <Card
+                                ref={props.formRef}
+                                className="w-full my-4 md:mt-2"
+                            >
+                                <CardContent className="mt-3">
+                                    <FormAccount
+                                        forms={props.data.category.forms}
+                                    />
+                                </CardContent>
+                            </Card>
+                        ) : null}
+                        <div ref={props.productListRef}>
+                            <ProductList
+                                number={props.data.category.forms ? 2 : 1}
+                                // category={data.category.name}
+                                nextRef={props.methodRef}
+                                products={props.products}
+                                // productSelected={data.product}
+                            />
+                        </div>
+                    </div>
+                    <div className="col-span-2">
+                        <div className="my-4 md:mt-2" ref={props.methodRef}>
+                            <Payment
+                                number={props.data.category.forms ? 3 : 2}
+                            />
+                        </div>
+                        <Card className="w-full my-4" ref={props.couponRef}>
+                            <CardContent>
+                                <div className="flex gap-2 items-center mt-3">
+                                    <div className="bg-theme-primary-100 p-2 w-7 h-7 flex justify-center items-center rounded-full">
+                                        <h4 className="font-bold rounded-full text-theme-primary-500">
+                                            {props.data.category.forms ? 4 : 3}
+                                        </h4>
+                                    </div>
+                                    <h4 className="font-medium ml-1">Promo</h4>
+                                </div>
+                                <Separator className="my-3" />
+                                <Promo
+                                    categoryUuid={props.data.category?.uuid}
+                                />
+                            </CardContent>
+                        </Card>
+                        <div ref={props.confirmationRef}>
+                            <FormConfirmation
+                                number={props.data.category.forms ? 5 : 4}
+                            />
+                        </div>
+                        <CheckoutAction
+                            confirmationRef={props.confirmationRef}
+                            formRef={props.formRef}
+                            paymentRef={props.methodRef}
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+}
+
+export default V2DetailCategory;
--- src/app/games/[slug]/header.tsx
+++ src/app/games/[slug]/header.tsx
@@ -6,51 +6,51 @@ import React from "react";
 import Description from "./collapsible-description";
 
 function Header({ category }: { category: IProductCategory }) {
-  return (
-    <Card className="w-full mt-2 h-full min-w-fit">
-      <CardContent className="p-0 pb-4">
-        <div
-          className="overflow-hidden relative w-full rounded-t-xl"
-          style={{ aspectRatio: 64 / 9 }}
-        >
-          <img
-            alt={category.name}
-            src={category?.image_url ?? "/assets/hero-games.svg"}
-            style={{ aspectRatio: 64 / 9 }}
-            className={`relative object-cover w-full rounded-t-xl blur-md`}
-          />
-          {/* <Image
+    return (
+        <Card className="w-full mt-2 h-full min-w-fit">
+            <CardContent className="p-0 pb-4">
+                <div
+                    className="overflow-hidden relative w-full rounded-t-xl max-h-24"
+                    style={{ aspectRatio: 64 / 9 }}
+                >
+                    <img
+                        alt={category.name}
+                        src={category?.image_url ?? "/assets/hero-games.svg"}
+                        style={{ aspectRatio: 64 / 9 }}
+                        className={`relative object-cover w-full rounded-t-xl blur-md`}
+                    />
+                    {/* <Image
             fill
             alt={category.name}
             src={"/assets/hero-games.svg"}
             style={{ aspectRatio: 64 / 9 }}
             className={`relative object-cover w-full rounded-t-xl`}
           /> */}
-        </div>
-        <div className="px-6">
-          <div className="flex -mt-4 ml-4 z-10 absolute items-end">
-            {category?.image_url ? (
-              <div className="border rounded flex items-center justify-center w-16 h-16 bg-white">
-                <img
-                  alt={category.name}
-                  className="h-auto w-14 rounded"
-                  src={category?.image_url}
-                />
-              </div>
-            ) : (
-              <div className="border rounded flex items-center justify-center w-16 h-16 bg-slate-200">
-                <CubeIcon className="w-10 h-10 text-white" />
-              </div>
-            )}
-            <div className="flex flex-col mb-2 ml-3">
-              <h5 className="font-bold">{category?.name}</h5>
-            </div>
-          </div>
-          <Description description={category?.description} />
-        </div>
-      </CardContent>
-    </Card>
-  );
+                </div>
+                <div className="px-6">
+                    <div className="flex -mt-4 ml-4 z-10 absolute items-end">
+                        {category?.image_url ? (
+                            <div className="border rounded flex items-center justify-center w-16 h-16 bg-white">
+                                <img
+                                    alt={category.name}
+                                    className="h-auto w-14 rounded"
+                                    src={category?.image_url}
+                                />
+                            </div>
+                        ) : (
+                            <div className="border rounded flex items-center justify-center w-16 h-16 bg-slate-200">
+                                <CubeIcon className="w-10 h-10 text-white" />
+                            </div>
+                        )}
+                        <div className="flex flex-col mb-2 ml-3">
+                            <h5 className="font-bold">{category?.name}</h5>
+                        </div>
+                    </div>
+                    <Description description={category?.description} />
+                </div>
+            </CardContent>
+        </Card>
+    );
 }
 
 export default Header;
