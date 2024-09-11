277e3e43edfbbafab6fbdbffadee74a2933046b8
Riza Bachtiar <riza.bachtiar.rb@gmail.com>
2024-09-11 11:32:45 +0700

    style: fix bg-color

--- src/app/flash-sale/page.tsx
+++ src/app/flash-sale/page.tsx
@@ -1,8 +1,6 @@
 "use client";
-import { Input } from "@/components/ui/input";
-import React, { ChangeEvent, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import FlashSaleCard from "./flash-sale-card";
-import { debounce } from "@/Helpers";
 import Image from "next/image";
 import Loading from "../loading";
 import { IFlashSaleInfo } from "@/types/flash-sale";
@@ -19,13 +17,11 @@ import {
 
 function Page() {
   const [data, setData] = useState<IFlashSaleInfo | undefined>();
-  const [search, setSearch] = useState("");
   const [loading, setLoading] = useState(true);
 
   const getFlashSale = async () => {
     let searchParams = new URLSearchParams({
       page_num: "1",
-      product_search: search,
     });
 
     setLoading(true);
@@ -47,11 +43,7 @@ function Page() {
     (async () => {
       await getFlashSale();
     })();
-  }, [search]);
-
-  const doSearch = debounce((e: ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-  }, 500);
+  }, []);
 
   return (
     <div className="md:pt-4 pb-4 px-2 flex w-full justify-center">
@@ -68,9 +60,15 @@ function Page() {
           </BreadcrumbList>
         </Breadcrumb>
         <div className="max-w-5xl w-full">
-          {data && data.products.length > 0 ? (
+          {loading ? (
+            <div
+              className={`container min-h-full flex items-center justify-center md:mx-2 px-4`}
+            >
+              <Loading />
+            </div>
+          ) : data ? (
             <>
-              <div className="flex px-4 sticky top-10 py-4 backdrop-blur-lg rounded-xl bg-background flex-col space-y-1.5 mb-3 z-10">
+              <div className="flex px-2 sticky top-10 py-4 backdrop-blur-lg rounded-xl bg-[#F0F8F6] flex-col space-y-1.5 mb-3 z-10">
                 <div className="flex items-center justify-between">
                   <p className="font-semibold text-lg flex">
                     <span className="mr-3">
@@ -85,45 +83,20 @@ function Page() {
                   </p>
                   <CountdownCard date={parseISO(data.expired_at)} />
                 </div>
-                <div className="flex space-x-1">
-                  <Input
-                    id="invoice"
-                    placeholder="Search..."
-                    className="bg-background"
-                    onChange={doSearch}
-                  />
-                </div>
               </div>
               <div className="min-h-[68vh]">
-                {loading ? (
-                  <Loading />
+                {data.products?.length > 0 ? (
+                  <div className="grid sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 grid-cols-2 gap-4 mx-2">
+                    {data.products.map((item, idx) => (
+                      <div className="w-full h-full" key={`${idx}`}>
+                        <FlashSaleCard data={item} />
+                      </div>
+                    ))}
+                  </div>
                 ) : (
-                  <>
-                    {data && data.products?.length > 0 ? (
-                      <div className="grid sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 grid-cols-2 gap-4 mx-2">
-                        {data.products.map((item, idx) => (
-                          <div className="w-full h-full" key={`${idx}`}>
-                            <FlashSaleCard data={item} />
-                          </div>
-                        ))}
-                      </div>
-                    ) : (
-                      <div className="flex flex-col items-center justify-center h-full w-full">
-                        {/* <Image
-                          src={
-                            "/assets/illustration/DrawKit Larry Character Illustration (10).svg"
-                          }
-                          className="opacity-50"
-                          alt="dw"
-                          height={400}
-                          width={400}
-                        /> */}
-                        <h5 className="text-xl font-bold">
-                          Tidak ada Flash Sale
-                        </h5>
-                      </div>
-                    )}
-                  </>
+                  <div className="flex flex-col items-center justify-center h-full w-full">
+                    <h5 className="text-xl font-bold">Tidak ada Flash Sale</h5>
+                  </div>
                 )}
               </div>
             </>
--- src/components/header/back-header.tsx
+++ src/components/header/back-header.tsx
@@ -8,7 +8,7 @@ function BackHeader({ title }: { title: string }) {
   const router = useRouter();
 
   return (
-    <header className="w-full flex md:hidden justify-center z-20 bg-white items-center top-0 sticky p-1 border-b h-[50px]">
+    <header className="w-full flex md:hidden justify-center z-20 bg-white items-center top-0 sticky p-1 h-[50px]">
       <div className="w-full flex max-w-7xl items-center justify-between">
         <div className="flex md:w-fit w-full justify-start items-center space-x-2 px-2">
           <ArrowLeftIcon
