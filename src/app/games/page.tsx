63dbf5e76f744418877636e471ddf8dc70318a31
Riza Bachtiar <riza.bachtiar.rb@gmail.com>
2024-09-25 09:55:29 +0700

    style: add padding bottom games

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -79,7 +79,7 @@ function Page() {
             <h1 className="font-semibold text-primary text-2xl">Produk</h1>
             <Filter onChange={setCategory} />
           </div>
-          <div className="min-h-[68vh] bg-background">
+          <div className="min-h-[68vh] bg-background pb-8">
             {loading ? (
               <Loading />
             ) : (
