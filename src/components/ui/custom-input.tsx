07a4a950981f34ae1da111ebc92dccd6f9d21aa4
Riza Bachtiar <riza.bachtiar.rb@gmail.com>
2024-10-18 07:33:27 +0700

    fix: fix passaword visible

--- src/components/ui/custom-input.tsx
+++ src/components/ui/custom-input.tsx
@@ -71,8 +71,6 @@ PhoneInputIndo.displayName = "PhoneInputIndo";
 const PasswordInput = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, ...props }, ref) => {
     const [showPassword, setShowPassword] = useState(false);
-    const disabled =
-      props.value === "" || props.value === undefined || props.disabled;
 
     return (
       <div className="relative">
@@ -88,9 +86,8 @@ const PasswordInput = React.forwardRef<HTMLInputElement, InputProps>(
           size="sm"
           className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
           onClick={() => setShowPassword((prev) => !prev)}
-          disabled={disabled}
         >
-          {showPassword && !disabled ? (
+          {showPassword ? (
             <EyeOpenIcon className="h-4 w-4" aria-hidden="true" />
           ) : (
             <EyeClosedIcon className="h-4 w-4" aria-hidden="true" />
